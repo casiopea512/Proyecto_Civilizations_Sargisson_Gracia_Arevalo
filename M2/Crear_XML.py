4dd1d26974404bac5c9ecca4fa56ca27bf7821c8
Arévalo 3 <martaarevaloprieto@gmail.com>
2024-05-07 20:08:42 +0200

    intento1

--- M2/Crear_XML.py
+++
@@ -1,5 +0,0 @@
-import pypyodbc as pyodbc
-
-cnxn = pyodbc.connect(
-'Trusted_Connection=yes;DRIVER={SQL Server};SERVER=localhost;DATABASE=HOLA;UID=sa;PWD=HOLAMUNDO'
-)
---
+++ M4/Crear_XML.py
@@ -0,0 +1,40 @@
+import pypyodbc as pyodbc
+#import oracledb as driverOracle
+
+print("Empieza conexción")
+
+#cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=localhost;DATABASE=Civilization;UID=admin;PWD=admin')
+# cursor = cnxn.cursor()
+# cursor.execute('select * from civilization_stats')
+#
+# for row in cursor.fetchall():
+#     print(row)
+
+#cnxn = driverOracle.connect(user="admin",password="admin",dsn='localhost/xepdb1')
+
+db_host = 'localhost'
+db_name = 'Civilization'
+db_user = 'admin'
+db_pswd = 'admin'
+
+connection_string = 'Driver={SQL Server};Server='+ db_host + ';Database='+db_name +';UID='+db_user +';PWD='+db_pswd +';'
+db = pyodbc.connect(connection_string)
+
+# query
+cursor = db.cursor()
+query = 'select * from civilization_stats'
+cursor.execute(query)
+
+while True:
+    row = cursor.fetchone()
+    if not row:
+        break
+    print("Respuesta: "+row)
+
+print("Conexión correcta")
+
+cursor.close()
+db.close()
+
+
+print("Fin conexión")
