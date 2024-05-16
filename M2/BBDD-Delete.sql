edca59fe468c188c202a750c644d07c682b9ae4b
WilliamSCastro <103841892+WilliamSCastro@users.noreply.github.com>
2024-05-16 22:22:37 +0200

    BBDD scripts modificados + Proc

---
+++ M2/BBDD-Delete.sql
@@ -0,0 +1,11 @@
+
+DELETE FROM civilization_stats;
+DELETE FROM attack_units_stats;
+DELETE FROM defense_units_stats;
+DELETE FROM special_units_stats;
+DELETE FROM battle_stats;
+DELETE FROM battle_log;
+DELETE FROM civilization_attack_stats;
+DELETE FROM civilization_defense_stats;
+DELETE FROM civilization_special_stats;
+DELETE FROM enemy_attack_stats;
