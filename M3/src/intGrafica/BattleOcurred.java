16b6971ca8d34b8b94f578f86e8c501bb2983fdf
Arévalo 3 <148625522+casiopea512@users.noreply.github.com>
2024-05-23 20:44:28 +0200

    Update BattleOcurred.java

--- M3/src/intGrafica/BattleOcurred.java
+++ M3/src/intGrafica/BattleOcurred.java
@@ -3,6 +3,7 @@ package intGrafica;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
